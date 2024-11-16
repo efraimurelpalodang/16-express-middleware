18f1fae9923b847345a43a54ea1d836ba5e1dbe9
efraim urel <efraimurelpalodang@gmail.com>
2024-11-16 08:10:38 +0800

    membuat folder

---
+++ app.js
@@ -0,0 +1,70 @@
+//! root slash(/) adalah root dari website kita
+//! rout adalah halaman utama aplikasinya
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//! memanggil express layout
+const expressLayouts = require('express-ejs-layouts');
+// untuk menggunakan express layoutnya
+app.use(expressLayouts);
+
+//? memberitahukan express kalo kita viewnya menggunakan ejs
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const mahasiswa = [
+    {
+      nama: "Efraim",
+      email: "hayoappa@gmail.com",
+    },
+    {
+      nama: "urel",
+      email: "gataumales@gmail.com",
+    },
+    {
+      nama: "Molly",
+      email: "hayamaja@gmail.com",
+    },
+    {
+      nama: "Ebong",
+      email: "apaliatliat@gmail.com",
+    },
+  ];
+
+  res.render("index", {
+    layout : 'layouts/main-layout',
+    nama: "efraim urel palodang",
+    tittle: "Halaman Home",
+    mahasiswa
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout : 'layouts/main-layout',
+    tittle : 'Halaman About'
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    layout : 'layouts/main-layout',
+    tittle : 'Halaman Contact'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`
+  );
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
